836121a2ee12fff74b612e49cd73125d402a2238
topce <topce@users.noreply.github.com>
2025-03-23 12:24:54 +0100

    tsc preserve comments by default

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ function handler(arg) {
 function doSomething(callback) {
     callback('hello', 42);
 }
+// Expected error because 'doSomething' wants a callback of
+// 2 parameters, but 'handler' only accepts 1
 doSomething(handler);
 class A {
     hi(a, b, c) {
@@ -19,11 +21,16 @@ class B {
     }
 }
 class BrokenUserService {
+    // TypeScript accepts this despite missing the required id parameter
     processUser(name) {
+        // This implementation never uses the id, which could cause logic errors
         console.log(`Processing user ${name}`);
+        // What if business logic depended on the id parameter?
     }
 }
 function requireBothParameters(callback) {
+    // This function assumes callback will use both parameters
     callback("test", 123);
 }
+// TypeScript allows this despite handler ignoring the second parameter
 requireBothParameters(handler);
