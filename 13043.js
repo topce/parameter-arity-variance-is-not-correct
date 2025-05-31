8400949b29130540c4d94abecdc4363e250365d2
topce <topce@users.noreply.github.com>
2025-05-31 22:13:22 +0200

    use my version of typescript https://www.npmjs.com/package/@topce/native-preview

--- 13043.js
+++
@@ -1,9 +0,0 @@
-const x = (a = 1) => a;
-const y = x;
-// TypeScript error: "Supplied parameters do not match signature of call target."
-// OK
-y('x').toFixed();
-const z = y;
-// No TypeScript error
-// Runtime error: Uncaught TypeError: z(...).toFixed is not a function
-z('x').toFixed();
--- 20541.js
+++
@@ -1,4 +0,0 @@
-const addOne = (effect) => {
-    effect(1, () => 'A');
-};
-addOne((arg1, arg2) => console.log(arg1 + 1));
